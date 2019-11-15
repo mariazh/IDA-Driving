a5f5d1cd775083dddd339d7787f62577223a41f3
Maria Zhang <mariaczhang@gmail.com>
2019-11-14 22:32:03 -0800

    Seperate dev and prod configs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bundle exec jekyll serve --port 3000 --host 0.0.0.0 --livereload
+bundle exec jekyll serve --port 3000 --host 0.0.0.0 --livereload --config _config.yml,_config_debug.yml
